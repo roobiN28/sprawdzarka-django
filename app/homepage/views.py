780e67f40c0093e42939c585368cbbb4a12205f6
Robert Zuba <robert.szymon.zuba@gmail.com>
2018-01-24 16:53:35 +0100

    add main template and navbar

--- app/homepage/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    param = request.GET.get('param')
-    return render(request, 'homepage/index.html', {'title': param, 'body': 'BODY Test: ' + param}, request)
--- app/urls.py
+++ app/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .homepage import views
+
+from app.controller import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test/', views.index)
+    path('', views.index)
 ]
