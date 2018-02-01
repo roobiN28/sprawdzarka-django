f0e69e3a249b56c23848d959367d1e6df6b480ed
Przemek <przemek.antoszek@gmail.com>
2018-02-01 16:57:50 +0100

    modifications in data base

--- app/controller/test.py
+++ app/controller/test.py
@@ -1,23 +1,27 @@
 import random
 
 from django.shortcuts import render
+from django.template import RequestContext
 
 from app.models import Test
 
 
 def add(request):
+    # context = RequestContext(request)
     if request.method == "POST":
-        # dodanie do bazy i zwrócenie
-        t = createTest()
+        t = Test()
+        t.test_name = 'dodaj'
+        t.input_data = 'input_data'
+        t.output_data = 'output_data'
         t.save()
-        return render(request, 'view/test/added.html', {'test': t})
+        return render(request,'view/test/added.html', {'test': t})
 
     else:
         return render(request, 'view/test/add.html')
 
 
-def createTest():
-    t = Test()
-    t.id = random.randint(0, 100)
-    t.input_data = 'input data'
-    t.output_data = 'output data'
+# def createTest():
+#     t = Test()
+#     t.id = random.randint(0, 100)
+#     t.input_data = 'input data'
+#     t.output_data = 'output data'
